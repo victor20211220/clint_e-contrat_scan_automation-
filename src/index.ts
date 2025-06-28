cf0fddb612b9851c203f35452fa6c696c82789b9
Victor <victor20211220@gmail.com>
2025-06-29 03:39:08 +0900

    - completed version

--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,6 @@ app.use('/api', router);
 // Connect to database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, '0.0.0.0', () => {
+  console.log('Server listening on port 5000');
+});
